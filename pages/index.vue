393b091794b0dfb72f62b908751cc24cf3995889
伊勢屋輝 <papvv@PapnoMacBook-Pro.local>
2021-03-09 10:05:02 +0900

    動きをつけた

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,10 @@
       </v-col>
       <v-col>
         <v-text-field
-          label="日時を入力"
-          :rules="rules"
-          hide-details="auto"
+          v-model="newDate"
+          label="ここに日付を"
+          solo
+          
         ></v-text-field>
       </v-col>
     </v-row>
@@ -18,16 +19,21 @@
       </v-col>
       <v-col>
         <v-text-field
-          label="タスクを入力"
-          :rules="rules"
-          hide-details="auto"
+          v-model="newTask"
+          label="ここにタスクを"
+          solo
+          @keydown.enter="create"
         > <template v-slot:append-outer>
-            <v-btn color="primary">SAVE</v-btn>
+            <v-btn color="primary"
+                   @click="create"
+            >SAVE</v-btn>
           </template>
         </v-text-field>
       </v-col>
     </v-row>
+
     <v-divider></v-divider>
+   
     <v-container fluid>
       <v-checkbox
         v-model="checkbox1"
@@ -38,31 +44,77 @@
         :label="`Checkbox 2`"
       ></v-checkbox>
     </v-container>
-    <v-card class="mx-auto" tile>
-      <v-list rounded>
-        <v-list-item-group color="primary">
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>TODO</v-list-item-title>
-            </v-list-item-content>
+
+    <v-divider/>
+
+    <v-card v-if="tasks.length > 0">
+      <v-slide-y-transition
+        class="py-0"
+        group
+        tag="v-list"
+      >
+        <template v-for="(task, i) in tasks">
+          <v-divider
+            v-if="i !== 0"
+            :key="`${i}-divider`"
+          ></v-divider>
+
+          <v-list-item :key="`${i}-${task.text}`">
+            <v-list-item-action>
+              <v-checkbox
+                v-model="task.done"
+                :color="task.done && 'grey' || 'primary'"
+              >
+                <template v-slot:label>
+                  <div
+                    :class="task.done && 'grey--text' || 'primary--text'"
+                    class="ml-4"
+                    v-text="task.text"
+                  ></div>
+                </template>
+              </v-checkbox>
+            </v-list-item-action>
           </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>TODODODO</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>TOTODO</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
+        </template>
+      </v-slide-y-transition>
     </v-card>
   </v-container>
   
 </template>
 
 <script>
+export default {
+    data: () => ({
+      tasks: [
+        {
+          done: false,
+          text: 'TODODODO',
+        },
+      ],
+      newTask: null,
+    }),
 
+    computed: {
+      completedTasks () {
+        return this.tasks.filter(task => task.done).length
+      },
+      progress () {
+        return this.completedTasks / this.tasks.length * 100
+      },
+      remainingTasks () {
+        return this.tasks.length - this.completedTasks
+      },
+    },
+
+    methods: {
+      create () {
+        this.tasks.push({
+          done: false,
+          text: this.newTask,
+        })
+
+        this.newTask = null
+      },
+    },
+  }
 </script>
