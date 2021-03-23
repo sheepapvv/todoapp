7eb7927aee3257fe4e70e578ea9eb8a22cb498b3
伊勢屋輝 <papvv@PapnoMacBook-Pro.local>
2021-03-23 23:32:51 +0900

    next

--- pages/index.vue
+++ pages/index.vue
@@ -6,16 +6,9 @@
       </v-col>
       <v-col>
         <v-text-field
-<<<<<<< HEAD
-          v-model="newDate"
-          label="ここに日付を"
-          solo
-          
-=======
           label="日時を入力"
           :rules="rules"
           hide-details="auto"
->>>>>>> main
         ></v-text-field>
       </v-col>
     </v-row>
@@ -25,33 +18,16 @@
       </v-col>
       <v-col>
         <v-text-field
-<<<<<<< HEAD
-          v-model="newTask"
-          label="ここにタスクを"
-          solo
-          @keydown.enter="create"
-        > <template v-slot:append-outer>
-            <v-btn color="primary"
-                   @click="create"
-            >SAVE</v-btn>
-=======
           label="タスクを入力"
           :rules="rules"
           hide-details="auto"
         > <template v-slot:append-outer>
             <v-btn color="primary">SAVE</v-btn>
->>>>>>> main
           </template>
         </v-text-field>
       </v-col>
     </v-row>
-<<<<<<< HEAD
-
     <v-divider></v-divider>
-   
-=======
-    <v-divider></v-divider>
->>>>>>> main
     <v-container fluid>
       <v-checkbox
         v-model="checkbox1"
@@ -62,41 +38,6 @@
         :label="`Checkbox 2`"
       ></v-checkbox>
     </v-container>
-<<<<<<< HEAD
-
-    <v-divider/>
-
-    <v-card v-if="tasks.length > 0">
-      <v-slide-y-transition
-        class="py-0"
-        group
-        tag="v-list"
-      >
-        <template v-for="(task, i) in tasks">
-          <v-divider
-            v-if="i !== 0"
-            :key="`${i}-divider`"
-          ></v-divider>
-
-          <v-list-item :key="`${i}-${task.text}`">
-            <v-list-item-action>
-              <v-checkbox
-                v-model="task.done"
-                :color="task.done && 'grey' || 'primary'"
-              >
-                <template v-slot:label>
-                  <div
-                    :class="task.done && 'grey--text' || 'primary--text'"
-                    class="ml-4"
-                    v-text="task.text"
-                  ></div>
-                </template>
-              </v-checkbox>
-            </v-list-item-action>
-          </v-list-item>
-        </template>
-      </v-slide-y-transition>
-=======
     <v-card class="mx-auto" tile>
       <v-list rounded>
         <v-list-item-group color="primary">
@@ -117,49 +58,10 @@
           </v-list-item>
         </v-list-item-group>
       </v-list>
->>>>>>> main
     </v-card>
   </v-container>
   
 </template>
 
 <script>
-<<<<<<< HEAD
-export default {
-    data: () => ({
-      tasks: [
-        {
-          done: false,
-          text: 'TODODODO',
-        },
-      ],
-      newTask: null,
-    }),
-
-    computed: {
-      completedTasks () {
-        return this.tasks.filter(task => task.done).length
-      },
-      progress () {
-        return this.completedTasks / this.tasks.length * 100
-      },
-      remainingTasks () {
-        return this.tasks.length - this.completedTasks
-      },
-    },
-
-    methods: {
-      create () {
-        this.tasks.push({
-          done: false,
-          text: this.newTask,
-        })
-
-        this.newTask = null
-      },
-    },
-  }
-=======
-
->>>>>>> main
 </script>
