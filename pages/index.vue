f3c36656b9b09d89e4eb3e6ffe1222c22c4dc7bd
sheepapvv <papruvv@gmail.com>
2021-03-30 09:54:26 +0900

    削除ボタンの追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,170 +1,112 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col>
-        <v-subheader>DUE DATE :</v-subheader>
-      </v-col>
-      <v-col>
-        <v-menu
-          v-model="menu2"
-          :close-on-content-click="false"
-          :nudge-right="40"
-          transition="scale-transition"
-          offset-y
-          min-width="auto"
+    <v-form ref="form" lazy-validation @submit.prevent="addTask">
+      <v-row>
+        <v-col>
+          <v-subheader>DUE DATE :</v-subheader>
+        </v-col>
+        <v-col>
+          <v-menu
+            v-model="menu2"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
           >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="date"
+                label="日時を選択"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
               v-model="date"
-              label="日時を選択"
-              prepend-icon="mdi-calendar"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker
-            v-model="date"
-            @input="menu2 = false"
-          ></v-date-picker>
-        </v-menu>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-subheader>TODO :</v-subheader>
-      </v-col>
-      <v-col>
-        <v-text-field
-          v-model="newTask"
-          label="ここにタスクを"
-          solo
-          @keydown.enter="create"
-        > <template v-slot:append-outer>
-            <v-btn color="primary"
-                   @click="create"
-            >SAVE</v-btn>
-          </template>
-        </v-text-field>
-      </v-col>
-    </v-row>
+              @input="menu2 = false"
+            ></v-date-picker>
+          </v-menu>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-subheader>TODO :</v-subheader>
+        </v-col>
+        <v-col>
+          <v-text-field v-model="newTask" label="ここにタスクを" solo>
+            <template v-slot:append-outer>
+              <v-btn color="primary" @click="create">SAVE</v-btn>
+            </template>
+          </v-text-field>
+        </v-col>
+      </v-row>
+    </v-form>
 
-    <v-divider/>
-   
+    <v-divider />
+
     <v-container>
       <v-row>
         <v-col>
-          <v-checkbox
-            v-model="inProgress"
-            :label="`IN PROGRESS`"
-          ></v-checkbox>
+          <v-checkbox v-model="inProgress" :label="`IN PROGRESS`"></v-checkbox>
         </v-col>
         <v-col>
-          <v-checkbox
-            v-model="finished"
-            :label="`FINISHED`"
-          ></v-checkbox>
+          <v-checkbox v-model="finished" :label="`FINISHED`"></v-checkbox>
         </v-col>
       </v-row>
     </v-container>
 
-    <v-divider/>
+    <v-divider />
+    <v-list two-line subheader>
+      <v-row>
+        <v-list-tile v-for="(task, index) in tasks" :key="index" avatar>
+          <v-list-item-content>
+            <v-list-item-title>{{ task.text }}</v-list-item-title>
+            <v-list-item-subtitle>{{ task.date }}</v-list-item-subtitle>
+          </v-list-item-content>
 
-    <v-card v-if="tasks.length > 0">
-      <v-slide-y-transition
-        class="py-0"
-        group
-        tag="v-list"
-      >
-        <template v-for="(task, i) in tasks">
-          <v-divider
-            v-if="i !== 0"
-            :key="`${i}-divider`"
-          ></v-divider>
-
-          <v-list-item :key="`${i}-${task.text}`">
-            <v-list-item-action>
-              <v-checkbox
-                v-model="task.done"
-                :color="task.done && 'grey' || 'primary'"
-              >
-                <template v-slot:label>
-                  <v-row>
-                    <v-col>
-                    <div
-                    :class="task.done && 'grey--text' || 'primary--text'"
-                    class="ml-4"
-                    v-text="task.date"
-                  ></div></v-col>
-                  
-                  <v-spacer/>
-                  
-                  <v-col>
-                    <v-chip
-                      class="ml-4"
-                      color="primary"
-                      label
-                      >IN PROGRESS
-                    </v-chip>
-                  </v-col>
-                    <v-col>
-                  <div
-                    :class="task.done && 'grey--text' || 'primary--text'"
-                    class="ml-4"
-                    v-text="task.text"
-                  ></div></v-col>
-                  <v-spacer/>
-                  <v-col>
-                    <v-btn color="primary"
-                    @click="finishedTodo"
-                    >FINISH</v-btn>
-                  </v-col>
-                  <v-col>
-                    <v-btn color="primary"
-                    @click="deleteTodo"
-                    >DELETE</v-btn>
-                  </v-col>
-                  </v-row>
-                </template>
-              </v-checkbox>
-            </v-list-item-action>
-          </v-list-item>
-        </template>
-      </v-slide-y-transition>
-    </v-card>
+          <v-spacer />
+          <v-col>
+            <v-btn color="primary" @click="finishedTodo">FINISH</v-btn>
+          </v-col>
+          <v-col>
+            <v-btn color="primary" @click="deleteTodo(index)">DELETE</v-btn>
+          </v-col>
+        </v-list-tile>
+      </v-row>
+    </v-list>
   </v-container>
-  
 </template>
 
 <script>
 export default {
-    data: () => ({
-      tasks: [
-        {
-          date: new Date().toISOString().substr(0, 10),
-          menu2: false,
-          text: 'TODODODO',
-        },
-      ],
-      newTask: null,
-    }),
-
-    
-
-    methods: {
-      create () {
-        this.tasks.push({
-          date: new Date().toISOString().substr(0, 10),
-          menu2: false,
-          text: this.newTask,
-        })
-
-        this.newTask = null
-        this.date = null
+  data: () => ({
+    tasks: [
+      {
+        date: new Date().toISOString().substr(0, 10),
+        menu2: false,
+        text: "TODODODO",
       },
-      deleteTodo: function (index) {
-        this.items.splice(index, 1)
-      },
+    ],
+    newTask: null,
+  }),
+
+  methods: {
+    create() {
+      this.tasks.push({
+        date: this.date,
+        menu2: false,
+        text: this.newTask,
+      });
+
+      this.newTask = null;
+      this.date = null;
     },
-  }
+    deleteTodo: function (index) {
+      this.tasks.splice(index, 1);
+    },
+  },
+};
 </script>
