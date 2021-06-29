d67b0246fc6b97ee41fdc078a77311a827ded413
sheepapvv <papruvv@gmail.com>
2021-06-30 01:28:19 +0900

    テスト

--- pages/index.vue
+++ pages/index.vue
@@ -46,38 +46,30 @@
     </v-form>
 
     <v-divider />
-    <v-divider />
+    
     <v-card>
       <v-card-title>
         タスク一覧
         <v-spacer></v-spacer>
         <v-item-group type="checkbox" v-model="search">
-      <!-- <v-checkbox
-        v-for="box in checkboxes"
-        :key="box.value"
-        :input-value="checkboxState[box.value]"
-        :label="box.text"
-      ></v-checkbox> -->
-       <v-checkbox
-          v-model="search"
-          label="IN PROGRESS"
-          value="IN PROGRESS"
-        ></v-checkbox>
-         <v-checkbox
-          v-model="search"
-          label="FINISHED"
-          value="FINISHED"
-        ></v-checkbox>
-    </v-item-group>
+          <v-checkbox
+            v-model="search"
+            label="IN PROGRESS"
+            value="IN PROGRESS"
+          ></v-checkbox>
+          <v-checkbox
+            v-model="search"
+            label="FINISHED"
+            value="FINISHED"
+          ></v-checkbox>
+        </v-item-group>
       </v-card-title>
-      <v-data-table
-        :headers="headers"
-        :items="desserts"
-        :search="search"
-      ><template template v-slot:[`item.actions`]="{ item }">>
-        <v-btn color="primary" @click="finishTodo(item)">FINISH</v-btn>
-        <v-btn color="primary" @click="deleteTodo(item)">DELETE</v-btn>
-      </template>
+      <v-data-table :headers="headers" :items="desserts" :search="search"
+        ><template template v-slot:[`item.actions`]="{ item }"
+          >>
+          <v-btn color="primary" @click="finishTodo(item)">FINISH</v-btn>
+          <v-btn color="primary" @click="deleteTodo(item)">DELETE</v-btn>
+        </template>
       </v-data-table>
     </v-card>
   </v-container>
@@ -86,32 +78,33 @@
 <script>
 export default {
   data: () => ({
-    search: '',
-      headers: [
-        {
-          text: 'タスク',
-          align: 'start',
-          sortable: false,
-          value: 'task',
-        },
-        { text: 'いつまで？', value: 'date' },
-        { text: '状態', value: 'state' },
-        { text: 'ボタン', value: 'actions', sortable: false },
-      ],
-      desserts: [
-        {
-          task: 'TOTOTOTODODODODODODODO',
-          date: new Date().toISOString().substr(0, 10),
-          state: 'IN PROGRESS',
-        },
-      ],
-    checkboxState: {
-      inprogress: true,
-      finished: true,
-    },
+    search: "",
+    headers: [
+      {
+        text: "タスク",
+        align: "start",
+        sortable: false,
+        value: "task",
+      },
+      { text: "いつまで？", value: "date" },
+      { text: "状態", value: "state" },
+      { text: "ボタン", value: "actions", sortable: false },
+    ],
+    desserts: [
+      {
+        task: "TOTOTOTODODODODODODODO",
+        date: new Date().toISOString().substr(0, 10),
+        state: "IN PROGRESS",
+      },
+      {
+        task: "これはふぃにっしゅ",
+        date: new Date().toISOString().substr(0, 10),
+        state: "FINISHED",
+      },
+    ],
     taskState: [
-      { value: 0, label: 'IN PROGRESS' },
-      { value: 1, label: 'FINISHED' },
+      { value: 0, label: "IN PROGRESS" },
+      { value: 1, label: "FINISHED" },
     ],
     newTask: null,
   }),
@@ -141,11 +134,11 @@ export default {
       this.date = null;
     },
     deleteTodo: function (item) {
-      const index = this.desserts.indexOf(item)
+      const index = this.desserts.indexOf(item);
       this.desserts.splice(index, 1);
     },
     finishTodo: function (desserts) {
-      desserts.state = desserts.state ? 0 : 1
+      desserts.state = desserts.state ? 0 : 1;
     },
   },
 };
